7e922bead60271f4d1ab9d3038fdbf1d724fb23c
柯立超 <ke.lichao@geely.com>
2017-05-26 17:03:08 +0800

    添加了组件案例

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/logo.png"><!-- 这个Logo每个路由都会展示 -->
     <router-view></router-view><!--  这里是用来展示路由页面内容的，如果想用跳转就用<router-link to='xxx'></router-link> -->
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 
-import Hello from '@/components/Hello'
-import Index from '@/components/Index'
-import aaa from '@/components/aaa'// 新添加的路由页面
+import Hello from '@/pages/Hello'
+import Index from '@/pages/Index'
+import aaa from '@/pages/aaa'// 新添加的路由页面
+import dialogPage from '@/pages/dialogPage'// 添加新组件
 
 const routes = [
     { path: '/', component: Hello },
     { path: '/aaa', component: aaa },// 新添加的路由配置
-    { path: '/index', component: Index }
+    { path: '/index', component: Index },
+    { path: '/dialogPage', component: dialogPage}
 ]
 
 export default routes
